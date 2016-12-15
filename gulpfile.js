4798c0608cd8d2b2d23a5c7fe5e975b8ad4cc7b0
Cristian Restituyo <cristian.restituyo@sdq.theoreminc.net>
2016-12-15 12:44:21 -0500

    Added gulp-imagemin task into the gulpfile along with the minimized versions of the resources.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    concat = require('gulp-concat')
-;
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin');
 
-function errorLog()
-{
+function errorLog() {
     console.error(e);
 }
-gulp.task('watch',function(){
-    gulp.watch('app/assets/sass/**/*.scss',['sass']);
-    gulp.watch('app/assets/js/**/*.js',['scripts']);
+
+gulp.task('watch', function() {
+    gulp.watch('app/assets/sass/**/*.scss', ['sass']);
+    gulp.watch('app/assets/js/**/*.js', ['scripts']);
+    gulp.watch('app/assets/imgs/**/', ['imagemin'])
 });
 
-gulp.task('sass',function(){
+gulp.task('sass', function() {
     return gulp.src('app/assets/sass/**/*.scss')
-    .pipe(sass())
-    .on('error',errorLog)
-    .pipe(gulp.dest('app/assets/css'))
+        .pipe(sass())
+        .on('error', errorLog)
+        .pipe(gulp.dest('app/assets/css'));
 });
 
-gulp.task('scripts',function(){
+gulp.task('imagemin', function() {
+    return gulp.src('app/assets/imgs/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('app/assets/imgs-opt'));
+});
+
+gulp.task('scripts', function() {
     return gulp.src('app/assets/js/**/*.{json,js}')
-          .pipe(concat('bundle.js'))
-          .on('error',errorLog)
-          .pipe(gulp.dest('app/assets/bundle.js'));
+        .pipe(concat('bundle.js'))
+        .on('error', errorLog)
+        .pipe(gulp.dest('app/assets/bundle.js'));
 });
 
-gulp.task('default',['sass','scripts','watch']);
+gulp.task('default', ['sass', 'scripts', 'imagemin', 'watch']);
